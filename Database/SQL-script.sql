9331a630186a89cf92154f37b479596d1dea9ac7
Jan Brebeck <Jan.Brebeck98@gmail.com>
2021-06-09 21:15:33 +0200

    added dummy data to sql-skript

--- Database/SQL-script.sql
+++ Database/SQL-script.sql
@@ -29,3 +29,13 @@ CREATE TABLE "BikesService" (
 ALTER TABLE "Bikes" ADD FOREIGN KEY ("Kunden_ID") REFERENCES "Kunden" ("Kunden_ID");
 
 ALTER TABLE "BikesService" ADD FOREIGN KEY ("Bike_ID") REFERENCES "Bikes" ("Bike_ID");
+
+-- Input dummydata
+INSERT INTO "Kunden"
+VALUES (01, 'Jan', 'Brebeck','S 6 15', TRUE,TO_DATE('17/12/2015', 'DD/MM/YYYY'),TO_DATE('17/12/2015', 'DD/MM/YYYY'));
+
+INSERT INTO "Bikes"
+VALUES (1, 01, 'MTB',TO_DATE('17/12/2015', 'DD/MM/YYYY'),'14.222;21.123','Kunde',TRUE);
+
+INSERT INTO "BikesService"
+VALUES (001, 'Reifenwechsel', 1,TO_DATE('17/12/2015', 'DD/MM/YYYY'),TO_DATE('17/12/2015', 'DD/MM/YYYY'));
