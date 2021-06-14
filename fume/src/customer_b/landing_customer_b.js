1137854d7fe09333a7ceae7c4d4d74f7be9a8b7b
Simon <s-scapan@web.de>
2021-06-14 18:49:36 +0200

    changed header

--- fume/src/customer_b/landing_customer_b.js
+++ fume/src/customer_b/landing_customer_b.js
@@ -3,35 +3,40 @@ import Salesdashboard from "./salesdashboard"
 import Customerdashboard from "./customerdashboard"
 import Workshop_planing from "./workshop_planing"
 import Order_planing from "./order_planing"
+import React, { Component }  from 'react';
+import Button from '@material-ui/core/Button';
+
 
 function Customer_B() {
   return (
-    <span>
-      <table>
-        <tr>
-          {/* Style Linien drum herum */}
-          <td>
-            <Link to="/customer_b/sales">Sales Dashboard</Link>
-          </td>
-          <td>
-            <Link to="/customer_b/customer">Kundenübersicht</Link>
-          </td>
-          <td>
-            <Link to="/customer_b/workshop_planing">Werkstattplanung</Link>
-          </td>
-          <td>
-            {/* englisches Wort? */}
-            <Link to="/customer_b/order_planing">Auftragsplanung</Link>
-          </td>
-        </tr>
-      </table>
-      <Switch>
-        <Route exact path="/customer_b/sales" component={Salesdashboard} />
-        <Route exact path="/customer_b/customer" component={Customerdashboard} />
-        <Route exact path="/customer_b/workshop_planing" component={Workshop_planing} />
-        <Route exact path="/customer_b/order_planing" component={Order_planing} />
-      </Switch>
-    </span>
+    <div>
+      <div>
+        <table align='right'>
+          <tr >
+            <td>
+              <Button component={Link} style={{ backgroundColor: "#ffd364", borderRadius: "30px"}} to="/customer_b/sales">Sales Dashboard</Button>
+            </td>
+            <td>
+              <Button component={Link} style={{ backgroundColor: "#ffd364", borderRadius: "30px"}} to="/customer_b/customer">Kundenübersicht</Button>
+            </td>
+            <td>
+              <Button component={Link} style={{ backgroundColor: "#ffd364", borderRadius: "30px"}} to="/customer_b/workshop_planing">Werkstattplanung</Button>
+            </td>
+            <td>
+              <Button component={Link} style={{ backgroundColor: "#ffd364", borderRadius: "30px"}} to="/customer_b/order_planing">Auftragsplanung</Button>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div>
+        <Switch>
+          <Route exact path="/customer_b/sales" component={Salesdashboard} />
+          <Route exact path="/customer_b/customer" component={Customerdashboard} />
+          <Route exact path="/customer_b/workshop_planing" component={Workshop_planing} />
+          <Route exact path="/customer_b/order_planing" component={Order_planing} />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
