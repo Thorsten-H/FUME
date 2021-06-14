a4f85282a840045ab39c1eb12febe13b3b7238a3
Simon <s-scapan@web.de>
2021-06-14 18:02:26 +0200

    Landing Page is reay

--- fume/src/Landing.js
+++ fume/src/Landing.js
@@ -1,12 +1,28 @@
 import { Link } from "react-router-dom";
 import './App.css';
+import React from 'react';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import logo from './FUME_logo.png';
 
 function App() {
     return (
-        <>
-            <Link to="/customer/">Customer</Link>
-            <Link to="/customer_b/">Business</Link>
-        </>
+        <div>
+            <div>
+                <br></br>
+                <img src={logo} alt="FUME-Logo" />
+            </div>
+            <div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <ButtonGroup size="large" color="#3f6d7b" aria-label="large outlined primary button group">
+                    <Button component={Link} style={{ backgroundColor: "#ffd364"}} to="/customer" >Customer</Button>
+                    <Button component={Link} style={{ backgroundColor: "#ffd364"}} to="/customer_b/" >Business</Button>
+                </ButtonGroup>
+            </div>
+            
+        </div>
     );
 }
 
