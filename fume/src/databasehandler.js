017e8403d8f1a9993582bac373a4652c91e77102
Jan Brebeck <Jan.Brebeck98@gmail.com>
2021-06-14 16:45:35 +0200

    hoffentlich ist jetzt das richtige geadded

--- fume/src/databasehandler.js
+++ fume/src/databasehandler.js
@@ -1,13 +1,81 @@
 import $ from 'jquery';
-const Database_url = "https://murat-db-default-rtdb.europe-west1.firebasedatabase.app/"
+const Database_url = "https://fume-db-default-rtdb.europe-west1.firebasedatabase.app/"
 
 
-export function getSalesData() {
+export function loadCustomer() {
     let response
     $.ajax({
-        url: Database_url + 'songs.json',
+        url: Database_url + 'customer.json',
         dataType: "json",
         type: 'GET',
+        async: false,
+        success: function (serverResponse) {
+            console.log(serverResponse)
+            response = serverResponse;
+        },
+        error: function (serverResponse) {
+            console.log("Errorlog: Response: ", serverResponse);
+            response = serverResponse;
+            debugger;
+            throw new Error("Error during loading of sales data");
+        }
+    });
+    return response
+}
+
+export function saveBike() {
+    let response
+    $.ajax({
+        url: Database_url + 'bikes.json',
+        dataType: "json",
+        data:  JSON.stringify({'test':13}),
+        type: 'POST',
+        async: false,
+        success: function (serverResponse) {
+
+            console.log(serverResponse)
+            response = serverResponse;
+        },
+        error: function (serverResponse) {
+            console.log("Errorlog: Response: ", serverResponse);
+            response = serverResponse;
+            debugger;
+            throw new Error("Error during loading of sales data");
+        }
+    });
+    return response
+}
+
+export function saveCustomer() {
+    let response
+    $.ajax({
+        url: Database_url + 'customer.json',
+        dataType: "json",
+        data:  JSON.stringify({'test':13}),
+        type: 'POST',
+        async: false,
+        success: function (serverResponse) {
+
+            console.log(serverResponse)
+            response = serverResponse;
+        },
+        error: function (serverResponse) {
+            console.log("Errorlog: Response: ", serverResponse);
+            response = serverResponse;
+            debugger;
+            throw new Error("Error during loading of sales data");
+        }
+    });
+    return response
+}
+
+export function saveBikeServices() {
+    let response
+    $.ajax({
+        url: Database_url + 'bikeServices.json',
+        dataType: "json",
+        data:  JSON.stringify({'test':13}),
+        type: 'POST',
         async: false,
         success: function (serverResponse) {
 
