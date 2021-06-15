6d8ca35ea9b650be397307d9537b77a65570ce3e
NWichter <niklaswichter@gmail.com>
2021-06-15 13:21:04 +0200

    Update customer_care.js

--- fume/src/customer/customer_care.js
+++ fume/src/customer/customer_care.js
@@ -1,9 +1,69 @@
 import React, { Component }  from 'react';
+import {Mail} from './../asset/mail.png'
+import {Phone} from './../asset/phone.png'
 
 function customer_care() {
+
   return (
-    <h1>customer_care</h1>
-  );
+    <h1> Care </h1>
+  )
 }
 
 export default customer_care;
+
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       name: '',
+//       subject: '',
+//       text: ''
+//     };
+
+//     this.handleInputChange = this.handleInputChange.bind(this);
+
+//   }
+
+//   handleInputChange(); {
+//     const target = target;
+//     const value = target.type === 'checkbox' ? target.checked : target.value;
+//     const name = target.name;
+
+//     this.setState({
+//       [name]: value
+//     });
+//   }
+
+//   render (); {
+//     return (
+      
+
+//       <div>
+
+//         <h1>Kontaktformular</h1>
+
+//         <div style={{ backgroundColor: "white", borderRadius: '10px', width: "80%"}}>
+//         <form onSubmit={this.handleSubmit}>
+//           <label>
+//             Name:
+//             <input type="text" name="name" />
+//           </label>
+//           <label>
+//             Betreff:
+//             <input type="text" name="subject" />
+//           </label>
+//           <label>
+//             Text:
+//             <input type="text" name="text" />
+//           </label>
+//           <input type="submit" value="Absenden" />
+//         </form>
+
+//         </div>
+
+//       </div>
+
+//     )
+//   }
+// }
+
+// export default customer_care;
