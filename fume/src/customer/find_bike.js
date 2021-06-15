8ed25e0f55592c9a655f44a41816f223325c8c62
Thorsten-H <thorsten.hilbradt@deutschebahn.com>
2021-06-15 21:13:44 +0200

    Karte funyt jetyt und custom icon auch

--- fume/src/customer/find_bike.js
+++ fume/src/customer/find_bike.js
@@ -1,12 +1,21 @@
 import React, { Component }  from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 import "./find_bike.css"
+import L from 'leaflet';
+//import marker from './Bike.png';
+import bike_logo from './../asset/Bike.png'
+
 
 function find_bike() {
   
   const bike_position = [49.4836, 8.477]
 
-  
+
+  const bikeIcon = new L.Icon({
+      iconUrl: bike_logo,
+      iconSize: [60,60],
+  });
+
   return (
 
     <div><h1>Find My Bike</h1><div id='mapid'>
@@ -15,7 +24,7 @@ function find_bike() {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-          <Marker position={bike_position}>
+          <Marker position={bike_position} icon={bikeIcon}>
             <Popup>
              Hier befindet sich Ihr Fahrrad!
             </Popup>
