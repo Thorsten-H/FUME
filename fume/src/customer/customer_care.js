923d8b937545fc65251f9afaeb616b9ec7421188
NWichter <niklaswichter@gmail.com>
2021-06-15 22:38:35 +0200

    added contact form

--- fume/src/customer/customer_care.js
+++ fume/src/customer/customer_care.js
@@ -1,69 +1,104 @@
 import React, { Component }  from 'react';
 import {Mail} from './../asset/mail.png'
 import {Phone} from './../asset/phone.png'
+import { TextField } from '@material-ui/core';
 
 function customer_care() {
-
+  
+  // data = {
+  //   name: "",
+  //   message: "",
+  //   email: "",
+  //   subject: "",
+  //   sent: false,
+  //   buttonText: "Send Message",
+  //   emailError: false,
+  // };
   return (
-    <h1> Care </h1>
+    <div>
+
+    <div>
+      <tr>
+
+      </tr>
+    </div>
+
+
+    <div>
+      <h2>
+        Kontaktformular
+      </h2>
+    </div>
+    
+    <form className="contact-form" onSubmit={(e) => {}}>
+    <br />
+    <br />
+    <br />
+    <TextField
+      id="outlined-basic"
+      placeholder="Enter your name"
+      label="Name"
+      variant="outlined"
+      //value={this.state.name}
+      //onChange={(e) => this.setState({ name: e.target.value })}
+      required
+      type="text"
+    />
+    <br />
+    <br />
+    <br />
+
+    <TextField
+      id="outlined-basic"
+      label="Email"
+      placeholder="Enter email address"
+      variant="outlined"
+      //value={this.state.email}
+      //onChange={(e) => this.handleChangeEmail(e)}
+      //error={this.state.emailError}
+      required
+      type="email"
+    />
+    <br />
+    <br />
+    <br />
+    <TextField
+      id="outlined-basic"
+      placeholder="Enter Subject"
+      label="Subject"
+      variant="outlined"
+      //value={this.state.subject}
+      //onChange={(e) => this.setState({ subject: e.target.value })}
+      required
+    />
+    <br />
+    <br />
+    <br />
+
+    <TextField
+      id="standard-multiline-flexible"
+      label="Message"
+      placeholder="Enter Message"
+      variant="outlined"
+      multiline
+      rowsMax={4}
+      //value={this.state.message}
+      //onChange={(e) => this.setState({ message: e.target.value })}
+      required
+      type="text"
+    />
+    <br />
+    <br />
+    <br />
+
+    <div className="button--container">
+      <button type="submit" className="button button-primary">
+        Submit
+      </button>
+    </div>
+  </form>  
+  </div>
   )
 }
 
 export default customer_care;
-
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       name: '',
-//       subject: '',
-//       text: ''
-//     };
-
-//     this.handleInputChange = this.handleInputChange.bind(this);
-
-//   }
-
-//   handleInputChange(); {
-//     const target = target;
-//     const value = target.type === 'checkbox' ? target.checked : target.value;
-//     const name = target.name;
-
-//     this.setState({
-//       [name]: value
-//     });
-//   }
-
-//   render (); {
-//     return (
-      
-
-//       <div>
-
-//         <h1>Kontaktformular</h1>
-
-//         <div style={{ backgroundColor: "white", borderRadius: '10px', width: "80%"}}>
-//         <form onSubmit={this.handleSubmit}>
-//           <label>
-//             Name:
-//             <input type="text" name="name" />
-//           </label>
-//           <label>
-//             Betreff:
-//             <input type="text" name="subject" />
-//           </label>
-//           <label>
-//             Text:
-//             <input type="text" name="text" />
-//           </label>
-//           <input type="submit" value="Absenden" />
-//         </form>
-
-//         </div>
-
-//       </div>
-
-//     )
-//   }
-// }
-
-// export default customer_care;
