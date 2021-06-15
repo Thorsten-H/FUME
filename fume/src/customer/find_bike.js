d6574650d1a1184080714e09448dce98bd951165
Thorsten-H <thorsten.hilbradt@deutschebahn.com>
2021-06-15 20:15:54 +0200

    erste version

--- fume/src/customer/find_bike.js
+++ fume/src/customer/find_bike.js
@@ -1,9 +1,27 @@
 import React, { Component }  from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import "./find_bike.css"
 
 function find_bike() {
+  
+  const bike_position = [49.4836, 8.477]
+
+  
   return (
-    <h1>find_bike</h1>
-  );
+
+    <div><h1>Find My Bike</h1><div id='mapid'>
+      <MapContainer center={bike_position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+          <Marker position={bike_position}>
+            <Popup>
+             Hier befindet sich Ihr Fahrrad!
+            </Popup>
+          </Marker>
+      </MapContainer>
+    </div></div>);
 }
 
 export default find_bike;
