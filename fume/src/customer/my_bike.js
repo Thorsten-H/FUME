0fd66cf7b1071bb01e75e0e67eb089cb1258d19f
NWichter <niklaswichter@gmail.com>
2021-06-15 16:59:34 +0200

    fix rectangle -> rect

--- fume/src/customer/my_bike.js
+++ fume/src/customer/my_bike.js
@@ -67,14 +67,14 @@ function my_bike() {
       <div>
         <img usemap="#bike" src={bike} width="735" height="439" alt="Fahrrad"></img>
         <map name ="bike">
-          <area id = "Vorderreifen" shape="rectangle" coords="57,325,251,376"  alt="Vorderreifen"></area>
-          <area id = "Frontlicht" shape="rectangle" coords="21,144,178,195"  alt="Vorderlicht"></area>
-          <area id = "Pedal" shape="rectangle" coords="305,349,431,402"  alt="Pedal"></area>
-          <area id = "Lenkrad" shape="rectangle" coords="19,28,162,77"  alt="Lenkrad"></area>
-          <area id = "Sattel" shape="rectangle" coords="527,21,653,75"  alt="Sattel"></area>
-          <area id = "Schaltung" shape="rectangle" coords="361,201,526,253"  alt="Schaltung"></area>
-          <area id = "Rücklicht" shape="rectangle" coords="546,148,706,201"  alt="Rücklicht"></area>
-          <area id = "Hinterreifen" shape="rectangle" coords="493,324,689,377"  alt="Hinterreifen"></area>
+          <area id = "Vorderreifen" shape="rect" coords="57,325,251,376"  alt="Vorderreifen"></area>
+          <area id = "Frontlicht" shape="rect" coords="21,144,178,195"  alt="Vorderlicht"></area>
+          <area id = "Pedal" shape="rect" coords="305,349,431,402"  alt="Pedal"></area>
+          <area id = "Lenkrad" shape="rect" coords="19,28,162,77"  alt="Lenkrad"></area>
+          <area id = "Sattel" shape="rect" coords="527,21,653,75"  alt="Sattel"></area>
+          <area id = "Schaltung" shape="rect" coords="361,201,526,253"  alt="Schaltung"></area>
+          <area id = "Rücklicht" shape="rect" coords="546,148,706,201"  alt="Rücklicht"></area>
+          <area id = "Hinterreifen" shape="rect" coords="493,324,689,377"  alt="Hinterreifen"></area>
         </map>
 
       </div>        
