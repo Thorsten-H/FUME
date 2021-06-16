68abcb0737ed45fe02e1fc8d0bd6000ef40166ba
NWichter <niklaswichter@gmail.com>
2021-06-16 11:41:47 +0200

    fields are now selectable

--- fume/src/customer/my_bike.js
+++ fume/src/customer/my_bike.js
@@ -54,6 +54,8 @@ function my_bike() {
   const DropdownExampleSelection = () => (
      <Dropdown
        placeholder='Fehlerquelle auswählen'
+       fluid
+       selection
        options={errorOptions}
      />
   )
@@ -67,14 +69,14 @@ function my_bike() {
       <div >
         <img usemap="#bike" src={bike} width="735" height="439" alt="Fahrrad" style={{borderRadius: '10px'}}/>
         <map name ="bike">
-          <area id = "Vorderreifen" shape="rect" coords="57,325,251,376"  alt="Vorderreifen"></area>
-          <area id = "Frontlicht" shape="rect" coords="21,144,178,195"  alt="Vorderlicht"></area>
-          <area id = "Pedal" shape="rect" coords="305,349,431,402"  alt="Pedal"></area>
-          <area id = "Lenkrad" shape="rect" coords="19,28,162,77"  alt="Lenkrad"></area>
-          <area id = "Sattel" shape="rect" coords="527,21,653,75"  alt="Sattel"></area>
-          <area id = "Schaltung" shape="rect" coords="361,201,526,253"  alt="Schaltung"></area>
-          <area id = "Rücklicht" shape="rect" coords="546,148,706,201"  alt="Rücklicht"></area>
-          <area id = "Hinterreifen" shape="rect" coords="493,324,689,377"  alt="Hinterreifen"></area>
+          <area id = "Vorderreifen" shape="rect" coords="57,325,251,376"  alt="Vorderreifen" href="#/customer/my_bike"></area>
+          <area id = "Frontlicht" shape="rect" coords="21,144,178,195"  alt="Vorderlicht" href="#/customer/my_bike"></area>
+          <area id = "Pedal" shape="rect" coords="305,349,431,402"  alt="Pedal" href="#/customer/my_bike"></area>
+          <area id = "Lenkrad" shape="rect" coords="19,28,162,77"  alt="Lenkrad" href="#/customer/my_bike"></area>
+          <area id = "Sattel" shape="rect" coords="527,21,653,75"  alt="Sattel" href="#/customer/my_bike"></area>
+          <area id = "Schaltung" shape="rect" coords="361,201,526,253"  alt="Schaltung" href="#/customer/my_bike"></area>
+          <area id = "Rücklicht" shape="rect" coords="546,148,706,201"  alt="Rücklicht" href="#/customer/my_bike"></area>
+          <area id = "Hinterreifen" shape="rect" coords="493,324,689,377"  alt="Hinterreifen" href="#/customer/my_bike"></area>
         </map>
 
       </div>        
