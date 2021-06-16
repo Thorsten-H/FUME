9dfc9913ee2d0846515474c13b434df4db774c31
Thorsten-H <thorsten.hilbradt@deutschebahn.com>
2021-06-16 19:11:55 +0200

    Seite nach Abspreche angepasst

--- fume/src/customer/customer_overview.js
+++ fume/src/customer/customer_overview.js
@@ -1,8 +1,27 @@
-import React, { Component }  from 'react';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+
+import siegfried from './../asset/Siegfried2.png'
+
 
 function customer_overview() {
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
   const customerData = [
-    { 'Kunden_ID': '14', 'Vorname': 'Jan', 'Nachname': 'Bern', 'Adresse': 'Q 7 68161 Mannheim', 'Aktiv': true, 'Beginndatum': '01.01.21', 'Enddatum': 'Datum' },
+    { 'Kunden_ID': '14', 'Vorname': 'Siegfried', 'Nachname': 'Bern', 'Alter':'40', 'Adresse': 'Steubenstraße 76 68169 Mannheim', 'Radius':'>10km', 'Kommunikation':'E-Mail, Telefon. Persönlich', 'Aktiv': true, 'Beginndatum': '01.01.2021', 'Enddatum': 'Datum' },
+    // { 'Kunden_ID': '45564351', 'Vorname': 'Niklas', 'Nachname': 'Müller', 'Adresse': 'Müllerstraße 555 64521 Groß Gerau', 'Aktiv': false, 'Beginndatum': 'Datum', 'Enddatum': 'Datum' },
+    // { 'Kunden_ID': '12345', 'Vorname': 'Simon', 'Nachname': 'Stein', 'Adresse': 'Q 8 68161 Mannheim', 'Aktiv': true, 'Beginndatum': 'Datum', 'Enddatum': 'Datum' },
+    // { 'Kunden_ID': '55', 'Vorname': 'Andres', 'Nachname': 'Gärtner', 'Adresse': 'Zeil 155 64135 Frankfurt', 'Aktiv': true, 'Beginndatum': 'Datum', 'Enddatum': 'Datum' },
+    // { 'Kunden_ID': '2345310', 'Vorname': 'Thorsten', 'Nachname': 'Becker', 'Adresse': 'Bäckerstraße 3 63215 Darmstadt', 'Aktiv': false, 'Beginndatum': 'Datum', 'Enddatum': 'Datum' }
   ]
 
   customerData.sort(function (a, b) {
@@ -19,57 +38,63 @@ function customer_overview() {
   customerData.forEach(e => {
     console.log(e.Aktiv)
     table_content.push(
-      <tr>
-        {/* <td className="Cell">
-          {e.Kunden_ID}
-        </td > */}
-        <td className="Cell"> 
+      <TableRow>
+        <TableCell align="center" component="th" scope="row">
           {e.Vorname}
-        </td>
-        <td className="Cell">
+        </TableCell>
+        <TableCell align="center" component="th" scope="row">
           {e.Nachname}
-        </td>
-        <td className="Cell">
+        </TableCell>
+        <TableCell align="center" component="th" scope="row">
+          {e.Alter}
+        </TableCell>
+        <TableCell align="center" component="th" scope="row">
           {e.Adresse}
-        </td>
-        <td className="Cell">
+        </TableCell>
+        <TableCell align="center" component="th" scope="row">
+          {e.Radius}
+        </TableCell>
+        <TableCell align="center" component="th" scope="row">
+          {e.Kommunikation}
+        </TableCell>
+        <TableCell align="center" component="th" scope="row">
           {e.Beginndatum}
-        </td>
-        {/* <td className="Cell">
-          {e.Enddatum}
-        </td>
-        <td className="Cell">
-          {e.Aktiv.toString()}
-        </td> */}
-      </tr>
+        </TableCell>
+      </TableRow>  
     )
   });
   console.log(table_content)
   return (
-    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '25vh'}}>
+    <div> 
+      <div width="20vh">
+        <br></br>
+        <br></br>
+        <TableContainer align="center" component={Paper}>
+          <Table className={useStyles.table} background-color="white" aria-label="simple table">
+            <TableHead>
+              <TableRow style={{ backgroundColor:'#ffd364'}}>
+                <TableCell align="center">Vorname</TableCell>
+                <TableCell align="center">Nachname</TableCell>
+                <TableCell align="center">Alter</TableCell>
+                <TableCell align="center">Adresse</TableCell>
+                <TableCell align="center">Radius</TableCell>
+                <TableCell align="center">Kommunikation</TableCell>
+                <TableCell align="center">Beginndatum</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {table_content}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
       <br></br>
-      <table style={{
-        borderCollapse: "separate",
-        backgroundColor: "white",
-        border: "solid black 1px",
-        borderRadius: "6px"
-  }}
+      <br></br>
+      <div>
+        <img  src={siegfried} width="400" height="300" alt="Siegfried" style={{borderRadius: '10px'}}/>
+      </div>
     
-    className="kudentable">
-      <tr>
-        {/* <th className="Cell">Kunden_ID</th> */}
-        <th className="Cell">Vorname</th>
-        <th className="Cell">Nachname</th>
-        <th className="Cell">Adresse</th>
-        <th className="Cell">Beginndatum</th>
-        {/* <th className="Cell">Enddatum</th>
-        <th className="Cell">Aktiv</th> */}
-      </tr>
-      {table_content}
-
-    </table>
     </div>
   );
 }
-
 export default customer_overview;
