aafc6a23294bf8c6f962ad8736418b5b9ba0d8b1
Thorsten-H <thorsten.hilbradt@deutschebahn.com>
2021-06-20 14:10:11 +0200

    Design angepasst

--- fume/src/customer/customer_overview.js
+++ fume/src/customer/customer_overview.js
@@ -1,9 +1,4 @@
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
+import {Table, TableHead, TableBody, TableCell, TableRow, TableContainer} from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
@@ -18,21 +13,12 @@ function customer_overview() {
     },
   });
   const customerData = [
-    { 'Kunden_ID': '14', 'Vorname': 'Siegfried', 'Nachname': 'Bern', 'Alter':'40', 'Adresse': 'Steubenstraße 76 68169 Mannheim', 'Radius':'>10km', 'Kommunikation':'E-Mail, Telefon. Persönlich', 'Aktiv': true, 'Beginndatum': '01.01.2021', 'Enddatum': 'Datum' },
+    { 'Kunden_ID': '14', 'Vorname': 'Siegfried', 'Nachname': 'Bern', 'Alter':'40', 'Adresse': 'Steubenstraße 76 68169 Mannheim', 'Radius':'>10km', 'Kommunikation':'E-Mail, Telefon, Persönlich', 'Aktiv': true, 'Beginndatum': '01.01.2021', 'Enddatum': 'Datum' },
     // { 'Kunden_ID': '45564351', 'Vorname': 'Niklas', 'Nachname': 'Müller', 'Adresse': 'Müllerstraße 555 64521 Groß Gerau', 'Aktiv': false, 'Beginndatum': 'Datum', 'Enddatum': 'Datum' },
     // { 'Kunden_ID': '12345', 'Vorname': 'Simon', 'Nachname': 'Stein', 'Adresse': 'Q 8 68161 Mannheim', 'Aktiv': true, 'Beginndatum': 'Datum', 'Enddatum': 'Datum' },
     // { 'Kunden_ID': '55', 'Vorname': 'Andres', 'Nachname': 'Gärtner', 'Adresse': 'Zeil 155 64135 Frankfurt', 'Aktiv': true, 'Beginndatum': 'Datum', 'Enddatum': 'Datum' },
     // { 'Kunden_ID': '2345310', 'Vorname': 'Thorsten', 'Nachname': 'Becker', 'Adresse': 'Bäckerstraße 3 63215 Darmstadt', 'Aktiv': false, 'Beginndatum': 'Datum', 'Enddatum': 'Datum' }
   ]
-
-  customerData.sort(function (a, b) {
-    if (a.Aktiv != b.Aktiv) {
-      return b.Aktiv ? 1 : -1;
-    }
-    if (a.Nachname != b.Nachname) {
-      return a.Nachname - b.Nachname; // descending
-    }
-  });
 
   let table_content = []
 
@@ -67,47 +53,70 @@ function customer_overview() {
   console.log(table_content)
   return (
     <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}> 
+      <table>
+        <tr>
+          <td>
+            <div>
+            <br></br>
+              <img id='siegfried' src={siegfried} width="120" height="120" alt="Siegfried" style={{borderRadius: '50%',
+                                                                                                   display:'inline-block', 
+                                                                                                   padding:'3px', 
+                                                                                                   float: 'center', 
+                                                                                                   borderStyle: 'solid', 
+                                                                                                   borderColor: '#ffffff', 
+                                                                                                   backgroundColor: '#ffffff',
+                                                                                                   marginTop: '10em' 
+                                                                                                    }} />
+             
+              <br />
+            </div>
+          </td>
+        </tr> 
+        <tr>
+          <td>
+          <div width="10px" style={{display:'inline-block', float:'left', textAlign:"right"}}>
+            <br></br>
+            <br></br>
+              <TableContainer align="center" component={Paper}>
+                <Table className={useStyles.table} background-color="white" aria-label="simple table">
+                  <TableHead>
+                    <TableRow style={{ backgroundColor:'#ffd364'}}>
+                      <TableCell align="center">Vorname</TableCell>
+                      <TableCell align="center">Nachname</TableCell>
+                      <TableCell align="center">Alter</TableCell>
+                      <TableCell align="center">Adresse</TableCell>
+                      <TableCell align="center">Radius</TableCell>
+                      <TableCell align="center">Kommunikation</TableCell>
+                      <TableCell align="center">Beginndatum</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {table_content}
+                  </TableBody>
+                </Table>
+              </TableContainer>
 
-      <div >
+          </div>
+          </td>
+        </tr>
         <br></br>
-        <br></br>
-        <img  src={siegfried} width="400" height="300" alt="Siegfried" style={{borderRadius: '10px', display:'inline-block', float: 'left'}}/>
-      </div>
-
-
-      <div width="10px" style={{display:'inline-block', float:'left', textAlign:"right"}}>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-          <TableContainer align="center" component={Paper}>
-            <Table className={useStyles.table} background-color="white" aria-label="simple table">
-              <TableHead>
-                <TableRow style={{ backgroundColor:'#ffd364'}}>
-                  <TableCell align="center">Vorname</TableCell>
-                  <TableCell align="center">Nachname</TableCell>
-                  <TableCell align="center">Alter</TableCell>
-                  <TableCell align="center">Adresse</TableCell>
-                  <TableCell align="center">Radius</TableCell>
-                  <TableCell align="center">Kommunikation</TableCell>
-                  <TableCell align="center">Beginndatum</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {table_content}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <Button style={{ width:'150px', height:"50px", backgroundColor: "red", color:"black", margin:"10px"}} variant="contained" color="primary">
-                  Abo pausieren
-                </Button>
-          <Button style={{ width:'150px', height:"50px", backgroundColor: "red", color:"black", margin:"10px"}} variant="contained" color="primary">
-                  kündigen
-                </Button>
-        </div>
-
-      </div>
+        <tr>
+          <td>
+            <div>
+            <Button style={{ width:'150px', height:"50px", backgroundColor: "#ffd364", color:"black", margin:"10px"}} variant="contained" color="primary">
+                Kontaktdaten ändern
+            </Button>
+            <Button style={{ width:'150px', height:"50px", backgroundColor: "red", color:"black", margin:"10px"}} variant="contained" color="primary">
+                      Abo pausieren
+            </Button>
+            <Button style={{ width:'150px', height:"50px", backgroundColor: "red", color:"black", margin:"10px"}} variant="contained" color="primary">
+                      kündigen
+            </Button>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </div>
   );
 }
 export default customer_overview;
