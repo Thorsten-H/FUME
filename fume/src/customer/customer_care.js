a47e10cc9db9593a9edfaf01a23ad5efd5e02064
Simon <s-scapan@web.de>
2021-06-22 14:24:21 +0200

    added functionality in myBike buttons to call dropdown

--- fume/src/customer/customer_care.js
+++ fume/src/customer/customer_care.js
@@ -1,5 +1,5 @@
 import "./customer_care.css"
-import React, { Component }  from 'react';
+import React from 'react';
 import Mail from './../asset/mail.png'
 import Phone from './../asset/phone.png'
 import {InputBase, makeStyles} from '@material-ui/core';
@@ -136,69 +136,3 @@ function customer_care() {
 }
 
 export default customer_care;
-
-
-
-
-
-
- {/* <TextField
-          id="outlined-basic"
-          placeholder="Enter your name"
-          label="Name"
-          variant="outlined"
-          style={{ border: '1px solid #ffd364', width:'500px', borderRadius:'5px'}}
-          //value={this.state.name}
-          //onChange={(e) => this.setState({ name: e.target.value })}
-          required
-          type="text"
-        />
-        <br />
-        <br />
-        <br /> */}
-
-        {/* <TextField
-          id="outlined-basic"
-          label="Email"
-          placeholder="Enter email address"
-          variant="outlined"
-          style={{ border: '1px solid #ffd364', width:'500px', borderRadius:'5px'}}
-          //value={this.state.email}
-          //onChange={(e) => this.handleChangeEmail(e)}
-          //error={this.state.emailError}
-          required
-          type="email"
-        />
-        <br />
-        <br />
-        <br />
-        <TextField
-          id="outlined-basic"
-          placeholder="Enter Subject"
-          label="Subject"
-          variant="outlined"
-          style={{ border: '1px solid #ffd364', width:'500px', borderRadius:'5px'}}
-          //value={this.state.subject}
-          //onChange={(e) => this.setState({ subject: e.target.value })}
-          required
-        />
-        <br />
-        <br />
-        <br />
-
-        <TextField
-          id="standard-multiline-flexible"
-          label="Message"
-          placeholder="Enter Message"
-          variant="outlined"
-          multiline
-          rowsMax={8}
-          style={{ border: '1px solid #ffd364', width:'500px', borderRadius:'5px'}}
-          //value={this.state.message}
-          //onChange={(e) => this.setState({ message: e.target.value })}
-          required
-          type="text"
-        />
-        <br />
-        <br />
-        <br /> */}
--- fume/src/customer/my_bike.js
+++ fume/src/customer/my_bike.js
@@ -1,122 +1,79 @@
 import "./my_bike.css"
 import React  from 'react';
 import bike from './../asset/bike_fume.png'
-import { Select, MenuItem, FormControl,InputLabel, makeStyles } from "@material-ui/core";
-import { TextField} from '@material-ui/core';
+import { Select, MenuItem, FormControl,InputLabel } from "@material-ui/core";
+import { TextField } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-import { mergeClasses } from "@material-ui/styles";
-
-
 
 function My_bike() {
+  const [defekt, setDefekt] = React.useState(0);
 
-  // const useStyles = makeStyles(theme => ({  
-  // formControl: {
-  //   minWidth: 100
-  //   }
-  // }))
-
-  //const classes = useStyles();
-  const [errorsource, setErrorsource] = React.useState('');
-  const handleChange = (event) => {
-    setErrorsource(event.target.value);
+  const handleChange = event => {
+    setDefekt(event.target.value);
   };
 
-
-  function clearData()
-  {  
-  document.getElementById('errordescription').value = "";
-  setErrorsource("")
-  }
-
-  function changeDropdown(e)
-  {
-  //setErrorsource(e);
-  }
-
-
   return (
-
-    
     <div> 
-
       <div >
         <img usemap="#bike" src={bike} width="735" height="439" alt="Fahrrad" style={{borderRadius: '10px', borderStyle: "solid", borderColor: '#ffd364', margin: '30px'}}/>
         <map name ="bike">
-          <area id = "Vorderreifen" shape="rect" coords="57,325,251,376" onClick={changeDropdown("Vorderreifen")}  alt="Vorderreifen" href="#/customer/my_bike"></area>
-          <area id = "Frontlicht" shape="rect" coords="21,144,178,195" onClick={changeDropdown("Frontlicht")} alt="Vorderlicht" href="#/customer/my_bike"></area>
-          <area id = "Pedal" shape="rect" coords="305,349,431,402" onClick={changeDropdown("Pedal")} alt="Pedal" href="#/customer/my_bike"></area>
-          <area id = "Lenkrad" shape="rect" coords="19,28,162,77" onClick={changeDropdown("Lenkrad")} alt="Lenkrad" href="#/customer/my_bike"></area>
-          <area id = "Sattel" shape="rect" coords="527,21,653,75" onClick={changeDropdown("Sattel")} alt="Sattel" href="#/customer/my_bike"></area>
-          <area id = "Schaltung" shape="rect" coords="361,201,526,253" onClick={changeDropdown("Schaltung")} alt="Schaltung" href="#/customer/my_bike"></area>
-          <area id = "Rücklicht" shape="rect" coords="546,148,706,201" onClick={changeDropdown("Rücklicht")} alt="Rücklicht" href="#/customer/my_bike"></area>
-          <area id = "Hinterreifen" shape="rect" coords="493,324,689,377" onClick={changeDropdown("Hinterreifen")} alt="Hinterreifen" href="#/customer/my_bike"></area>
+          <area id = "Vorderreifen" shape="rect" coords="57,325,251,376"  value={1} onClick={handleChange} alt="Vorderreifen"></area>
+          <area id = "Frontlicht"   shape="rect" coords="21,144,178,195"  value={2} onClick={handleChange} alt="Vorderlicht"></area>
+          <area id = "Pedal"        shape="rect" coords="305,349,431,402" value={3} onClick={handleChange} alt="Pedal"></area>
+          <area id = "Lenkrad"      shape="rect" coords="19,28,162,77"    value={4} onClick={handleChange} alt="Lenkrad"></area>
+          <area id = "Sattel"       shape="rect" coords="527,21,653,75"   value={5} onClick={handleChange} alt="Sattel"></area>
+          <area id = "Schaltung"    shape="rect" coords="361,201,526,253" value={6} onClick={handleChange} alt="Schaltung"></area>
+          <area id = "Rücklicht"    shape="rect" coords="546,148,706,201" value={7} onClick={handleChange} alt="Rücklicht"></area>
+          <area id = "Hinterreifen" shape="rect" coords="493,324,689,377" value={8} onClick={handleChange} alt="Hinterreifen"></area>
         </map>
-
       </div>        
 
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-
         <div style={{backgroundColor: "white", borderRadius: '10px', width: '800px', display: 'flex',  justifyContent:'center', padding: '10px', alignItems:'center'}}>
-         <h3>
-          Fehlermeldung anlegen
-        </h3>         
+          <h3>
+            Fehlermeldung anlegen
+          </h3>         
           <div>
-          <form  className="contact-form" onSubmit={(e) => {}} >
-            <table cellspacing="5" cellpadding="8" align="center" valign="top">
-              <tr>
-              <br />
-              <td>
-                <FormControl className={mergeClasses.formControl} style={{width:"150px"}}>
-                  <InputLabel>Fehlerquelle</InputLabel>
-                  <Select 
-                  value={errorsource}
-                  onChange={handleChange}>
-                    <MenuItem value={"Vorderreifen"}>Vorderreifen</MenuItem>
-                    <MenuItem value={"Frontlicht"}>Frontlicht</MenuItem>
-                    <MenuItem value={"Pedal"}>Pedal</MenuItem>
-                    <MenuItem value={"Lenkrad"}>Lenkrad</MenuItem>
-                    <MenuItem value={"Sattel"}>Sattel</MenuItem>
-                    <MenuItem value={"Schaltung"}>Schaltung</MenuItem>
-                    <MenuItem value={"Rücklicht"}>Rücklicht</MenuItem>
-                    <MenuItem value={"Hinterreifen"}>Hinterreifen</MenuItem>
-                    <MenuItem value={"Diebstahl"}>Diebstahl</MenuItem>
-                    <MenuItem value={"Sonstiges"}> Sonstiges</MenuItem>
-                  </Select>
-                 </FormControl>
-                </td>
+            <form  className="contact-form" onSubmit={(e) => {}} >
+              <table cellspacing="5" cellpadding="8" align="center" valign="top">
+                <tr>
+                  <br />
+                  <td>
+                    <FormControl style={{width:"150px"}}>
+                      <InputLabel id="defekt-label">Fehlerquelle</InputLabel>
+                      <Select labelId="defekt-label" id="defekt-select" value={defekt} onChange={handleChange}>
+                        <MenuItem value={1}>Vorderreifen</MenuItem>
+                        <MenuItem value={2}>Frontlicht</MenuItem>
+                        <MenuItem value={3}>Pedal</MenuItem>
+                        <MenuItem value={4}>Lenkrad</MenuItem>
+                        <MenuItem value={5}>Sattel</MenuItem>
+                        <MenuItem value={6}>Schaltung</MenuItem>
+                        <MenuItem value={7}>Rücklicht</MenuItem>
+                        <MenuItem value={8}>Hinterreifen</MenuItem>
+                        <MenuItem value={9}>Diebstahl</MenuItem>
+                        <MenuItem value={0}>Sonstiges</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </td>
 
-              <td><TextField
-                id="errordescription"
-                placeholder="Fehlerbeschreibung einfügen"
-                label="Fehlerbeschreibung"
-                variant="outlined"
-                style={{width:'300px', color:'red'}}
-                //value={this.state.name}
-                //onChange={(e) => this.setState({ name: e.target.value })}
-                required
-                type="text"
-              />
-              </td>
-              <br />
-              <td>
-              <div className="button--container">
-                <Button onClick= {clearData} style={{ width:'70px', backgroundColor: "#ffd364", color:"black"}} variant="contained" color="primary">
-                  Submit
-                </Button>
-              </div>
-              </td>
-              </tr>
-            </table> 
-          </form>
+                  <td>
+                    <TextField id="errordescription" placeholder="Fehlerbeschreibung einfügen" label="Fehlerbeschreibung" variant="outlined" style={{width:'300px', color:'red'}} required type="text"/>
+                  </td>
+                  <br />
+                  <td>
+                    <div className="button--container">
+                      <Button onClick={handleChange} style={{ width:'70px', backgroundColor: "#ffd364", color:"black"}} variant="contained" color="primary">
+                        Submit
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              </table> 
+            </form>
           </div>
         </div>
-
-        
       </div>
     </div>
-
-  
   );
 }
 
